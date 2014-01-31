0acecaee4c665b9fbb536535552596d8dc6a32c6
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2014-01-31 07:06:12 +0100

    initial commit of latex template files for blackarch documents

---
+++ templates/latex/gen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ${#} -ne 1 ]
+then
+    echo "sh ${0} <action>"
+    echo "actions:"
+    echo "  pdf     - create pdf document"
+    echo "  html    - create one html document"
+    echo "  clean   - clean files and directory"
+    exit 1337
+fi
+
+if [ "${1}" = "pdf" ]
+then
+    pdflatex template.tex
+elif [ "${1}" = "html" ]
+then
+    latex2html -nonavigation -notop_navigation -nobottom_navigation \
+        -split 0 -toc_depth 10 -lcase_tags -address "BlackArch Linux" \
+        -show_section_numbers -html_version "4.0" -info 0 template.tex
+elif [ "${1}" = "clean" ]
+then
+    rm -rf *.pdf *.toc *.log *.aux *.out *.exs template/
+else
+    echo "mount /dev/brain"
+    exit 1337
+fi
